8155b8d977e49741df150241bdd0b718ac4f0161
watanabe3tipapa <twpoet@nifty.com>
2026-08-01 21:13:37 +0900

    fix: GITHUB_REPO をデモリポジトリの実値に設定

--- src/consts.ts
+++ src/consts.ts
@@ -3,6 +3,6 @@ export const SITE_TITLE = "issues-astro-cms";
 export const SITE_DESCRIPTION =
   "GitHub Issues / Discussions を CMS にした Astro ブログのテンプレート";
 
-export const GITHUB_REPO = "USERNAME/issues-astro-cms";
+export const GITHUB_REPO = "watanabe3tipapa/issues-astro-cms";
 
 export const GITHUB_DISCUSSIONS_BASE_URL = `https://github.com/${GITHUB_REPO}/discussions`;
